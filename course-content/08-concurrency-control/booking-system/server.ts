54e4a15ffd5005bb3bfd3ebb48694e01f2dce9a2
Hayoun Lee <sidhlee@gmail.com>
2022-10-25 08:10:33 -0400

    add GET and PUT controller

--- course-content/08-concurrency-control/booking-system/server.ts
+++ course-content/08-concurrency-control/booking-system/server.ts
@@ -20,6 +20,41 @@ app.get('/', (req, res) => {
   return res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/seats', async (req, res) => {
+  const result = await pool.query('select * from seats');
+  res.send(result.rows);
+});
+
+app.put('/:id/:name', async (req, res) => {
+  try {
+    const { id, name } = req.params;
+    const connection = await pool.connect();
+
+    // Begin transaction
+    await connection.query('BEGIN');
+    const result = await connection.query(
+      'SELECT * FROM seats where id = $1 and is_booked = False FOR UPDATE',
+      [id]
+    );
+    if (result.rowCount === 0) {
+      return res.send({ error: 'Seat already booked' });
+    }
+
+    const updateResult = await connection.query(
+      'UPDATE seats SET is_booked = True, name = $2 where id = $1',
+      [id, name]
+    );
+
+    await connection.query('COMMIT');
+    connection.release();
+
+    return res.send(updateResult);
+  } catch (err) {
+    console.log(err);
+    res.send(500);
+  }
+});
+
 app.listen(port, () => {
   console.log('listening on: ' + port);
 });
